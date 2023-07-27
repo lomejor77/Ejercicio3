8a639d42c852540e120e8e255be49542606ebdc0
Alex Muñoz <lomejor77@gmail.com>
2023-07-27 09:54:12 -0400

    Ejercicio 1 Room se incorporan dos fragmentos y navigation

--- app/src/main/java/cl/awakelabs/room/MainActivity.kt
+++ app/src/main/java/cl/awakelabs/room/MainActivity.kt
@@ -2,7 +2,19 @@ package cl.awakelabs.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+/*
+[x] Navigation
+[x ] Framento Agregar
+[x ] Crear Interfaz UI (boton textView Etc)
+[X] Room
+    [X ] _Entity
+    [X ] _Dao
+    [X ] _Base Datos
 
+[ ] Fragmento listado
+[ ] RecyclerView
+
+*/
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
